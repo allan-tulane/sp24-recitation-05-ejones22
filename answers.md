1a398619e2d07d9d7366a878ae80f2a3685a21e8
ejones22 <143116175+ejones22@users.noreply.github.com>
2024-03-21 04:55:10 +0000

    final push

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Reciation 5
 ## Answers
 
-**Name:**_________________________
+**Name:**___Emma Jones_____
 
 
 Place all written answers from `recitation-05.md` here for easier grading.
@@ -13,8 +13,45 @@ Place all written answers from `recitation-05.md` here for easier grading.
 
 
 - **1b.**
+performance is influenced by how the elements are distributed within the list. If the
+list is already sorted, Quicksort with a fixed pivot might demonstrate better
+performance because it capitalizes on the pre-sorted nature of lists, which aligns
+with its selection strategy. However, Quicksort with a random pivot may encounter
+worst-case scenarios where the randomly selected pivot partitions the array unevenly.
+In the case of a reverse-sorted list, Quicksort with a fixed pivot may perform
+reasonably well, but selecting the pivot as the first or last element may not yield
+optimal results. Conversely, Quicksort with a random pivot might exhibit superior
+performance in reverse-sorted lists since it's less likely to choose a pivot from the
+extremes.
 
 
+Sorted List:
+|      n |qsort-fixed-pivot(ms)|qsort-random-pivot(ms)|timsort(ms)|
+|--------|---------------------|----------------------|-----------|
+|    100 |               0.077 |                0.214 |     0.054 |
+|    200 |               0.086 |                0.342 |     0.091 |
+|    500 |               0.104 |                1.198 |     0.087 |
+|   1000 |               0.194 |               42.881 |     0.097 |
+|   2000 |               0.252 |                4.705 |     0.108 |
+|   5000 |               0.583 |               22.558 |     0.105 |
+|  10000 |               1.005 |               78.310 |     0.158 |
+|  20000 |               2.189 |              114.577 |     0.313 |
+|  50000 |               5.999 |              374.949 |     0.884 |
+| 100000 |              11.793 |              985.816 |     1.465 |
 
+Random List:
+|      n |qsort-fixed-pivot(ms)|qsort-random-pivot(ms)|timsort(ms)|
+|--------|---------------------|----------------------|-----------|
+|    100 |               0.261 |                0.256 |     0.075 |
+|    200 |               0.671 |                0.474 |     0.123 |
+|    500 |               1.092 |                1.415 |     0.111 |
+|   1000 |               2.037 |                2.221 |     0.182 |
+|   2000 |               4.689 |                4.597 |     0.336 |
+|   5000 |              11.021 |               70.320 |     0.878 |
+|  10000 |              24.016 |               94.420 |     1.947 |
+|  20000 |             130.491 |              190.174 |     4.398 |
+|  50000 |             406.376 |              473.544 |    11.824 |
+| 100000 |            1005.399 |              992.076 |    82.722 |
 
 - **1c.**
+as input size grows, timsort is most efficient followed by fixed-pivot
--- main.py
+++ main.py
@@ -1,81 +1,109 @@
 import random, time
 import tabulate
+from functools import reduce
 
-def ssort(L):
-    ### selection sort
-    if (len(L) == 1):
-        return(L)
-    else:
-        m = L.index(min(L))
-        print('selecting minimum %s' % L[m])       
-        L[0], L[m] = L[m], L[0]
-        print('recursively sorting L=%s\n' % L[1:])
-        return [L[0]] + selection_sort(L[1:])
-        
+
+def select_random(a):
+  return random.choice(a)
+
+def select_first(a):
+  return a[0]
+
+
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
-    
+  ## To Do
+  if len(a) <= 1:
+    return a
+  else:
+    p = pivot_fn(a)
+    l = list(filter(lambda x: x < p, a))
+    r = list(filter(lambda x: x > p, a))
+    c = list(filter(lambda x: x==p, a))
+    return qsort(l, pivot_fn) + c + qsort(r, pivot_fn)
+
 def time_search(sort_fn, mylist):
-    """
-    Return the number of milliseconds to run this
-    sort function on this list.
+        """
+        Return the number of milliseconds to run this
+        search function on this list.
 
-    Note 1: `sort_fn` parameter is a function.
-    Note 2: time.time() returns the current time in seconds. 
-    You'll have to multiple by 1000 to get milliseconds.
+        Note 1: `sort_fn` parameter is a function.
+        Note 2: time.time() returns the current time in seconds.
+        You'll have to multiple by 1000 to get milliseconds.
 
-    Params:
-      sort_fn.....the search function
-      mylist......the list to search
-      key.........the search key 
+        Params:
+        sort_fn.....the search function
+        mylist......the list to search
+        key.........the search key
 
-    Returns:
-      the number of milliseconds it takes to run this
-      search function on this input.
-    """
-    start = time.time()
-    sort_fn(mylist)
-    return (time.time() - start) * 1000
-    ###
+          Returns:
+            the number of milliseconds it takes to run this
+            search function on this input.
+          """
+        start = time.time()
+        sort_fn(mylist)
+        return (time.time() - start) * 1000
+          ###
 
 def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
-    """
-    Compare the running time of different sorting algorithms.
-
-    Returns:
-      A list of tuples of the form
-      (n, linear_search_time, binary_search_time)
-      indicating the number of milliseconds it takes
-      for each method to run on each value of n
-    """
-    ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
-    result = []
-    for size in sizes:
-        # create list in ascending order
-        mylist = list(range(size))
-        # shuffles list if needed
-        #random.shuffle(mylist)
-        result.append([
-            len(mylist),
-            time_search(qsort_fixed_pivot, mylist),
-            time_search(qsort_random_pivot, mylist),
-        ])
+  """
+  Compare the running time of linear_search and binary_search
+  for input sizes as given. The key for each search should be
+   -1. The list to search for each size contains the numbers from 0 to n-1,
+  sorted in ascending order.
+  
+  You'll use the time_search function to time each call.
+  
+  Returns:
+  A list of tuples of the form
+  (n, linear_search_time, binary_search_time)
+  indicating the number of milliseconds it takes
+  for each method to run on each value of n
+  """
+ ### TODO
+  quicksort_fixed_pivot = lambda a: qsort(a, select_first)
+  quicksort_random_pivot = lambda a: qsort(a, select_random)
+  tim_sort = sorted
+  result = []
+  for size in sizes:
+    mylist = list(range(int(size)))
+    random.shuffle(mylist)
+    result.append([
+    len(mylist),
+    #time_search(selection_sort, mylist),
+    time_search(quicksort_fixed_pivot, mylist),
+    time_search(quicksort_random_pivot, mylist),
+    time_search(tim_sort, mylist)
+            ])
     return result
-    ###
+          ###
 
 def print_results(results):
-    """ change as needed for comparisons """
-    print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
-                            floatfmt=".3f",
-                            tablefmt="github"))
+   """ done """
+   print(tabulate.tabulate(results,
+   headers=['n', 'quicksort fixed pivot', 'quicksort random pivot', 'tim-sort'],
+   floatfmt=".3f",
+   tablefmt="github"))
+
+
+def plot_results(results):
+  import matplotlib.pyplot as plt
+  plt.figure(figsize=(8,6))
+  plt.plot([i[0] for i in results], [i[1] for i in results], 'o-', label='quicksort-fixed')
+  plt.plot([i[0] for i in results], [i[2] for i in results], 'o-', label='quicksort-random')
+  plt.plot([i[0] for i in results], [i[3] for i in results], 'o-', label='timsort')
+  plt.xlabel('n')
+  plt.ylabel('milliseconds')
+  plt.legend(fontsize=20)
+  plt.savefig('results.pdf')
+
+
 
 def test_print():
-    print_results(compare_sort())
+  results = compare_sort()
+  print_results(results)
+  plot_results(results)
 
-random.seed()
-test_print()
+  random.seed()
+  test_print()
+
+print(test_print())
